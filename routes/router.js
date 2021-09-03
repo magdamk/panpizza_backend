86083d9997ebe3d54c91055ede89fb6a6bb2409e
magdamk <75847988+magdamk@users.noreply.github.com>
2021-09-03 17:46:25 +0200

    some unnecessary lines removed

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-require('dotenv').config();
-const bcrypt = require('bcryptjs');
-const uuid = require('uuid');
-const jwt = require('jsonwebtoken');
+//const User = require('../models/user');
+//const bcrypt = require('bcryptjs');
+//const uuid = require('uuid');
+//const jwt = require('jsonwebtoken');
 
 const userMiddleware = require('../middleware/users.js');
 const loginController = require('../controllers/login_controller');
@@ -12,4 +11,5 @@ const loginController = require('../controllers/login_controller');
 router.post('/sign-up', userMiddleware.validateRegister, loginController.postSignUp);
 router.post('/login', loginController.postLogin);
 router.get('/secret-route', userMiddleware.isLoggedIn, loginController.getSecretRoute);
+
 module.exports = router;
