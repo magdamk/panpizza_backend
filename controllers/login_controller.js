bf1d69f75bf6a965fcd21bf0391ca7f88c8e216f
magdamk <75847988+magdamk@users.noreply.github.com>
2021-11-04 19:28:56 +0100

    console.log clean-up

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -97,7 +97,6 @@ exports.postLogin = async(req, res, next) => {
                         expiresIn: process.env.EXPIRE
                     }
                 );
-                //  console.log(checkUser[0]._id);
                 checkUser[0].last_login = new Date();
                 const updatedUserLogin = await checkUser[0].save()
                 res.status(201).send({ msg: 'Logged in!', token, email: checkUser[0].email, role: checkUser[0].role, id: checkUser[0]._id, phone: checkUser[0].phone });
